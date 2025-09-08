53f372f3dd22c02bcfb1619ffac1b876b61df558
rahuldev75 <cserahuldev075@gmail.com>
2025-09-08 07:51:41 +0530

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Rahul Dev Nayak on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
